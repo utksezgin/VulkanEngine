38ca7ec993dbfbabdaea60a71e5d510ebd57410f
Utku Sezgin <utksezgin@gmail.com>
2021-01-03 23:36:34 +0300

    VulkanRenderer Class

---
+++ VulkanRenderer.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <stdexcept>
+#include <vector>
+#include <iostream>
+
+class VulkanRenderer
+{
+	VulkanRenderer();
+	~VulkanRenderer();
+};
+
